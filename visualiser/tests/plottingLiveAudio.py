11190372234dfaca59eb6ec8ef75659a256ad63a
hazza <harrystenholm@yahoo.com>
2026-02-27 21:48:55 +0000

    basic visualising system audio

---
+++ visualiser/tests/plottingLiveAudio.py
@@ -0,0 +1,44 @@
+import librosa
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
+import numpy as np
+import pyaudio
+
+#Config
+format = pyaudio.paInt16
+channels = 2
+rate = 44100
+chunk = 1024*channels
+index = 9
+
+#define plot
+fig, ax = plt.subplots()
+x = np.arange(0, chunk * channels, 1)
+y = np.zeros_like(x)
+line, = ax.plot(x, y, '-', lw=1)
+
+ax.set_ylim(-32768, 32768)
+ax.set_xlim(0, chunk * channels)
+plt.setp(ax, xticks=[0, chunk], yticks=[-32768, 0, 32768])
+plt.show(block=False)
+
+if __name__ == "__main__":
+    p = pyaudio.PyAudio()
+    stream = p.open(
+        format = format,
+        channels = channels,
+        rate = rate,
+        input = True,
+        input_device_index=index,
+        frames_per_buffer = chunk
+    )
+
+    while plt.fignum_exists(fig.number):
+        data = np.frombuffer(stream.read(chunk), dtype = np.int16)
+        line.set_ydata(data)
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+
+stream.stop_stream()
+stream.close()
+p.terminate()
